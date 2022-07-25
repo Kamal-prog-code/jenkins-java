a55e8d482b448dacf1fdfa891dd04da2bd5fffbe
Kamal-prog-code <karrerajisyam123@gmail.com>
2022-07-25 19:31:50 +0530

    add all file

---
+++ src/main/java/com/devops/sampleDevops/DevopsAutoApplication.java
@@ -0,0 +1,13 @@
+package com.devops.sampleDevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsAutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/sampleDevops/DevopsAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.sampleDevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
